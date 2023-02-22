491977a8f8547b7ecf48082c3f72e9b3c74b3b26
ilyab <i-baykov@internet.ru>
2023-02-23 01:40:34 +0300

    Добавил меню

--- horoscope/url.py
+++ horoscope/url.py
@@ -3,6 +3,7 @@ from django.urls import path
 from horoscope import views as horoscope_views
 
 urlpatterns = [
+    path('menu', horoscope_views.menu),
     path('<int:current_zodiac_int>', horoscope_views.horoscope_info_digit),
     path('<str:current_zodiac_str>', horoscope_views.horoscope_info, name="url_current_zodiac"),
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -32,6 +32,15 @@ zodiac_dict = {
 zodiac_list = list(zodiac_dict)
 
 
+def menu(requst):
+    result = "<ol>"
+    for sign in zodiac_dict:
+        link = reverse("url_current_zodiac", args=(sign,))
+        result += f"<li><a href = '{link}'>{sign.title()}</a></li>"
+    result += "<ol>"
+    return HttpResponse(f"{result}")
+
+
 def horoscope_info(request, current_zodiac_str):
     if zodiac_dict.get(current_zodiac_str):
         current_symbol = zodiac_dict[current_zodiac_str]
@@ -43,5 +52,5 @@ def horoscope_info(request, current_zodiac_str):
 
 def horoscope_info_digit(requese, current_zodiac_int):
     if len(zodiac_list) >= current_zodiac_int > 0:
-        link = reverse("url_current_zodiac", args=(zodiac_list[current_zodiac_int-1],))
+        link = reverse("url_current_zodiac", args=(zodiac_list[current_zodiac_int - 1],))
         return HttpResponseRedirect(link)
